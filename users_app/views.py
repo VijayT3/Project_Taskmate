ff08cea476335b0a595bc0e76ca676af558a1330
VijayT3 <4v3tandale@gmail.com>
2023-01-07 17:26:08 +0530

    registration form

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        register_form = CustomRegisterForm(request.POST)
+    #  content in form
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(
+                request, ("New User Created Successfully! Login to get started."))
+            return redirect('register')
+    else:
+        register_form = CustomRegisterForm(request.POST)
+    return render(request, 'register.html', {'register_form': register_form})
